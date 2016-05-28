96898ba30ff70231e07ed0dcc288154ba8f6a745
swares <waresscott@gmail.com>
2016-05-28 15:44:25 -0600

    allow 5 gpio to be controlled instead of two

--- init.lua
+++ init.lua
@@ -36,11 +36,17 @@ tmr.alarm(0, 1000, 1, function()
 end)
 -- wifi connect end
 
-
-led1 = 3
-led2 = 4
+-- config / start httpd server
+led0 = 3 // internal
+led1 = 4
+led2 = 5
+led3 = 6
+led4 = 7
+gpio.mode(led0, gpio.OUTPUT)
 gpio.mode(led1, gpio.OUTPUT)
 gpio.mode(led2, gpio.OUTPUT)
+gpio.mode(led3, gpio.OUTPUT)
+gpio.mode(led4, gpio.OUTPUT)
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
@@ -56,10 +62,17 @@ srv:listen(80,function(conn)
             end
         end
         buf = buf.."<!doctype html><title>Greenhouse Control</title><h1>Greenhouse Control</h1>";
-        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
+        buf = buf.."<p>GPIO0 <a href=\"?pin=ON0\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF0\"><button>OFF</button></a></p>";
+        buf = buf.."<p>GPIO1 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
         buf = buf.."<p>GPIO2 <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>";
+        buf = buf.."<p>GPIO3 <a href=\"?pin=ON3\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF3\"><button>OFF</button></a></p>";
+        buf = buf.."<p>GPIO4 <a href=\"?pin=ON4\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF4\"><button>OFF</button></a></p>";
         local _on,_off = "",""
-        if(_GET.pin == "ON1")then
+        if(_GET.pin == "ON0")then
+              gpio.write(led0, gpio.HIGH);
+        elseif(_GET.pin == "OFF0")then
+              gpio.write(led0, gpio.LOW);
+        elseif(_GET.pin == "ON1")then
               gpio.write(led1, gpio.HIGH);
         elseif(_GET.pin == "OFF1")then
               gpio.write(led1, gpio.LOW);
@@ -67,6 +80,14 @@ srv:listen(80,function(conn)
               gpio.write(led2, gpio.HIGH);
         elseif(_GET.pin == "OFF2")then
               gpio.write(led2, gpio.LOW);
+        elseif(_GET.pin == "ON3")then
+              gpio.write(led3, gpio.HIGH);
+        elseif(_GET.pin == "OFF3")then
+              gpio.write(led3, gpio.LOW);
+        elseif(_GET.pin == "ON4")then
+              gpio.write(led4, gpio.HIGH);
+        elseif(_GET.pin == "OFF4")then
+              gpio.write(led4, gpio.LOW);
         end
         client:send(buf);
         client:close();
