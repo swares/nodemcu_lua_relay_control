fd8273ba02844a3caa0383849bb120ef5f72006c
swares <waresscott@gmail.com>
2016-05-28 15:35:16 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,10 +11,8 @@
 //const char web_index[]      PROGMEM  = { "<!DOCTYPE html><html><head><title><title><head><body><h1>Greenhouse Control Station - Index</h1><p>GPIO0 <a href='?pin=ON1'><button>ON</button></a>&nbsp;<a href='?pin=OFF1'><button>OFF</button></a></p><p>GPIO2 <a href='?pin=ON2'><button>ON</button></a>&nbsp;<a href='?pin=OFF2'><button>OFF</button></a></p>";
 //const char web_index_ajax[] PROGMEM  = { "<!DOCTYPE html><html><head><title>Greenhouse Control Station - Index</title><script>function GetSwitchState() {nocache = '&nocache=' + Math.random() * 1000000;var request = new XMLHttpRequest();request.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {if (this.responseText != null) {document.getElementById('switch_txt'.innerHTML = this.responseText;}}}};request.open('GET', 'ajax_switch' + nocache, true);request.send(null);setTimeout('GetSwitchState()', 1000);}</script></head><body onload='GetSwitchState()'><h1>Environmental Monitoring Station</h1><p id='switch_txt'>Switch state: Not requested...</p></body></html>" };
 
-// HTML5 Minium valid: <!doctype html><title> </title> 
-
 -- Configure Wireless Internet
-print('\nAll About Circuits init.lua\n')
+print('\nGreenhouse Control\n')
 wifi.setmode(wifi.STATION)
 print('set mode=STATION (mode='..wifi.getmode()..')\n')
 print('MAC Address: ',wifi.sta.getmac())
