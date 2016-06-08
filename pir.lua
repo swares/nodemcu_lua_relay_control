db0e2c2d2a95da55344e4b9e2a57f43486f1d2c4
swares <waresscott@gmail.com>
2016-06-08 17:27:07 -0600

    Create pir.lua

---
+++ pir.lua
@@ -0,0 +1,14 @@
+function motion()
+  print("Motion Detection : ON!") 
+  stat = "ON" 
+  gpio.write(outpin,gpio.HIGH)  -- Led ON - Motion detected
+  gpio.trig(6,"down",nomotion)  -- trigger on falling edge
+  return stat
+end
+function nomotion()
+  print("Motion Detection : OFF!") 
+  stat = "OFF" 
+  gpio.write(outpin,gpio.LOW)   -- Led OFF 
+  gpio.trig(6,"up",motion)  -- trigger on rising edge
+  return stat
+end
