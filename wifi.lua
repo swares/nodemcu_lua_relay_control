7270e16a224112ae4a8c8676e242905353e90839
swares <waresscott@gmail.com>
2016-06-08 17:28:00 -0600

    Create wifi.lua

---
+++ wifi.lua
@@ -0,0 +1,23 @@
+-- Configure Wireless Internet
+print('\nGreenhouse Control\n')
+wifi.setmode(wifi.STATION)
+print('set mode=STATION (mode='..wifi.getmode()..')\n')
+print('MAC Address: ',wifi.sta.getmac())
+print('Chip ID: ',node.chipid())
+print('Heap Size: ',node.heap(),'\n')
+-- wifi config start
+wifi.sta.config('Linksys',"YOUR_NETWORK_PASSWORD")
+-- wifi config end
+-- wifi connect start
+tmr.alarm(0, 1000, 1, function()
+   if wifi.sta.getip() == nil then
+      print("Connecting to AP...\n")
+   else
+      ip, nm, gw=wifi.sta.getip()
+      print("IP Info: \nIP Address: ",ip)
+      print("Netmask: ",nm)
+      print("Gateway Addr: ",gw,'\n')
+      tmr.stop(0)
+   end
+end)
+-- wifi connect end
