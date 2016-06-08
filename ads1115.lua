b11b4762ac1df353443cbcace6c587797f5f260f
swares <waresscott@gmail.com>
2016-06-08 17:35:50 -0600

    Create ads1115.lua

---
+++ ads1115.lua
@@ -0,0 +1,130 @@
+ADS1015_REG_POINTER_MASK            = 0x03
+ADS1015_REG_POINTER_CONVERT         = 0x00
+ADS1015_REG_POINTER_CONFIG          = 0x01
+ADS1015_REG_POINTER_LOWTHRESH       = 0x02
+ADS1015_REG_POINTER_HITHRESH        = 0x03
+
+ADS1015_REG_CONFIG_MUX_SINGLE_0     = 0x4000
+ADS1015_REG_CONFIG_MUX_SINGLE_1     = 0x5000
+ADS1015_REG_CONFIG_MUX_SINGLE_2     = 0x6000
+ADS1015_REG_CONFIG_MUX_SINGLE_3     = 0x7000
+
+ADS1015_REG_CONFIG_OS_SINGLE        = 0x8000
+
+ADS1015_REG_CONFIG_CQUE_NONE        = 0x0003
+ADS1015_REG_CONFIG_CLAT_NONLAT      = 0x0000
+ADS1015_REG_CONFIG_CPOL_ACTVLOW     = 0x0000
+ADS1015_REG_CONFIG_CMODE_TRAD       = 0x0000
+ADS1015_REG_CONFIG_DR_1600SPS       = 0x0080
+ADS1015_REG_CONFIG_MODE_SINGLE      = 0x0100
+
+
+-- Functions
+function readRegister(address, reg)
+    i2c.start(0)
+    i2c.address(0, address, i2c.TRANSMITTER)
+    i2c.write(0, reg)
+    i2c.stop(0)
+
+    i2c.start(0)
+    i2c.address(0, address, i2c.RECEIVER)
+    local all = i2c.read(0, 2)
+    i2c.stop(0)
+
+
+    local a = string.byte(all)
+    if a == nil then
+        a = 0
+    end
+
+    local b = string.byte(all, 2)
+    if b == nil then
+        b = 0
+    end
+
+    local ret = 0
+    ret = bit.bor(ret, a)
+    ret = bit.lshift(ret, 8)
+    ret = bit.bor(ret, b)
+
+    return ret
+end
+
+function writeRegister(address, reg, value)
+    i2c.start(0)
+    i2c.address(0, address, i2c.TRANSMITTER)
+    i2c.write(0, reg)
+    local high = 0
+    high = bit.rshift(value, 8)
+    i2c.write(0, high)
+    local low = 0
+    low = bit.band(0xFF)
+    i2c.write(0, low)
+    i2c.stop(0)
+end
+
+
+function readADC_SingleEnded(channel)
+    if channel > 3 then
+        return 0
+    end
+
+
+    local config = 0
+    config = bit.bor(config, ADS1015_REG_CONFIG_CQUE_NONE)
+    config = bit.bor(config, ADS1015_REG_CONFIG_CLAT_NONLAT)
+    config = bit.bor(config, ADS1015_REG_CONFIG_CPOL_ACTVLOW)
+    config = bit.bor(config, ADS1015_REG_CONFIG_CMODE_TRAD)
+    config = bit.bor(config, ADS1015_REG_CONFIG_DR_1600SPS)
+    config = bit.bor(config, ADS1015_REG_CONFIG_MODE_SINGLE)
+
+    if channel == 0 then
+        config = bit.bor(config, ADS1015_REG_CONFIG_MUX_SINGLE_0)
+    elseif channel == 1 then
+        config = bit.bor(config, ADS1015_REG_CONFIG_MUX_SINGLE_1)
+    elseif channel == 2 then
+        config = bit.bor(config, ADS1015_REG_CONFIG_MUX_SINGLE_2)
+    elseif channel == 3 then
+        config = bit.bor(config, ADS1015_REG_CONFIG_MUX_SINGLE_3)
+    end
+
+    config = bit.bor(config, ADS1015_REG_CONFIG_OS_SINGLE)
+
+    writeRegister(ADDR, ADS1015_REG_POINTER_CONFIG, config)
+
+    tmr.delay(ADS1115_CONVERTIONDELAY)
+
+    local ret = readRegister(ADDR, ADS1015_REG_POINTER_CONVERT)
+
+    return ret
+end
+
+print('I2C_SETUP...')
+i2c.setup(0, SDA, SCL, i2c.SLOW)
+print('SET UP')
+
+function main()
+    local a = readADC_SingleEnded(0)
+    if a == nil then
+        a = 0
+    end
+    local b = readADC_SingleEnded(1)
+    if b == nil then
+        b = 0
+    end
+    local c = readADC_SingleEnded(2)
+    if c == nil then
+        c = 0
+    end
+    local d = readADC_SingleEnded(3)
+    if d == nil then
+        d = 0
+    end
+    print("--------")
+    print("1: " .. a)
+    print("2: " .. b)
+    print("3: " .. c)
+    print("4: " .. d)
+end
+
+tmr.alarm(0, 1000, 1, function() main() end )
