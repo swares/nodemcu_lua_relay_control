b086eddf7c5170449103ea48df436795f8b5d212
swares <waresscott@gmail.com>
2016-06-08 19:07:14 -0600

    Update dht11.lua

--- dht11.lua
+++ dht11.lua
@@ -97,28 +97,3 @@ print ("Humidity: "..Humidity.."."..HumidityDec)
 print ("ChecksumReceived: "..Checksum)
 print ("ChecksumTest: "..ChecksumTest)
 end
-
---- Get temp and send data to thingspeak.com
-function sendData()
-getTemp()
--- conection to thingspeak.com
-print("Sending data to thingspeak.com")
-conn=net.createConnection(net.TCP, 0) 
-conn:on("receive", function(conn, payload) print(payload) end)
--- api.thingspeak.com 184.106.153.149
-conn:connect(80,'184.106.153.149') 
-conn:send("GET /update?key=XXXXXXXXXX&field1="..Temperature.."."..TemperatureDec.."&field2="..Humidity.."."..HumidityDec.." HTTP/1.1\r\n") 
-conn:send("Host: api.thingspeak.com\r\n") 
-conn:send("Accept: */*\r\n") 
-conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
-conn:send("\r\n")
-conn:on("sent",function(conn)
-                      print("Closing connection")
-                      conn:close()
-                  end)
-conn:on("disconnection", function(conn)
-                      print("Got disconnection...")
-  end)
-end
--- send data every X ms to thing speak
-tmr.alarm(2, 60000, 1, function() sendData() end )
